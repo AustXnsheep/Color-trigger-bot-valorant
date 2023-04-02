5b3369a7c3609aa4ffe35a8b62874e9754233be5
AustXnsheep <110955845+AustXnsheep@users.noreply.github.com>
2023-04-01 18:39:17 -0700

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,178 @@
+import ctypes
+import time
+import PIL.Image
+import PIL.ImageGrab
+import keyboard
+import mss
+import winsound
+from tkinter import *
+
+S_HEIGHT, S_WIDTH = PIL.ImageGrab.grab().size
+PURPLE_R, PURPLE_G, PURPLE_B = (250, 100, 250)
+TOLERANCE = 60
+GRABZONE = 5
+GRABZONEUP = "ctrl + up"
+GRABZONEDOWN = "ctrl + down"
+TRIGGER_KEY = "ctrl + alt"
+COLOR_KEY = "alt"
+MODECHANGEKEY = "ctrl + tab"
+CURRENTMODE = "SLOW"
+
+
+def click():
+    try:
+        text_info_1 = str(text1.get())
+        text_info_2 = str(text2.get())
+    except Exception as e:
+        text1.delete(0, END)
+        text2.delete(0, END)
+        text3.delete(0, END)
+        text3.insert(0, f'Error: {e}')
+        return
+    text3.delete(0, END)
+    text3.insert(0, str(text_info_1) + str(text_info_2))
+
+#GUI CORE
+root = Tk()
+root.geometry('300x400')
+root.title('Valorant TriggerBot - Made by Austxnsheep')
+
+# GUIS ELEMENTS
+label1 = Label(root, text='VERSION 1.1')
+label1.pack()
+
+spacing1 = Label(root)
+spacing1.pack()
+
+text1 = Entry(root)
+text1.pack(ipadx=20)
+
+spacing2 = Label(root, text='+')
+spacing2.pack()
+
+text2 = Entry(root)
+text2.pack(ipadx=20)
+
+spacing3 = Label(root)
+spacing3.pack()
+
+button = Button(root, text='Update', command=click)
+button.pack()
+
+spacing4 = Label(root)
+spacing4.pack()
+
+text3 = Entry(root)
+text3.pack(ipadx=60)
+
+#TRIGGERBOT
+class FoundEnemy(Exception):
+    pass
+
+class TriggerBot:
+    def __init__(self):
+        self.toggled = False
+        self.mode = 0
+        self.last_reac = 0
+
+    def toggle(self):
+        self.toggled = not self.toggled
+
+    def switch(self):
+        if self.mode != 2:
+            self.mode += 1
+        else:
+            self.mode = 0
+        if self.mode == 0:
+            CURRENTMODE = "SLOW (0.5/CPS)"
+            winsound.Beep(200, 200)
+        if self.mode == 1:
+            CURRENTMODE = "MEDIUM (0.25/CPS)"
+            winsound.Beep(500, 200)
+        if self.mode == 2:
+            CURRENTMODE = "FAST (0.12/CPS)"
+            winsound.Beep(700, 200)
+            time.sleep(0.1)
+        print_banner(self)
+
+    def click(self):
+        ctypes.windll.user32.mouse_event(2, 0, 0, 0, 0)
+        time.sleep(0.25)
+        ctypes.windll.user32.mouse_event(4, 0, 0, 0, 0)
+
+    def approx(self, r, g, b):
+        return PURPLE_R - TOLERANCE < r < PURPLE_R + TOLERANCE and PURPLE_G - TOLERANCE < g < PURPLE_G + TOLERANCE and PURPLE_B - TOLERANCE < b < PURPLE_B + TOLERANCE
+
+    def grab(self):
+        with mss.mss() as sct:
+            bbox = (int(S_HEIGHT / 2 - GRABZONE), int(S_WIDTH / 2 - GRABZONE), int(S_HEIGHT / 2 + GRABZONE),
+                    int(S_WIDTH / 2 + GRABZONE))
+            sct_img = sct.grab(bbox)
+            return PIL.Image.frombytes('RGB', sct_img.size, sct_img.bgra, 'raw', 'BGRX')
+
+    def scan(self):
+        start_time = time.time()
+        pmap = self.grab()
+        try:
+            for x in range(0, GRABZONE * 2):
+                for y in range(0, GRABZONE * 2):
+                    r, g, b = pmap.getpixel((x, y))
+                    if self.approx(r, g, b):
+                        raise FoundEnemy
+        except FoundEnemy:
+            self.last_reac = int((time.time() - start_time) * 1000)
+            self.click()
+            if self.mode == 0:
+                time.sleep(0.5)
+            if self.mode == 1:
+                time.sleep(0.25)
+            if self.mode == 2:
+                time.sleep(0.12)
+            print_banner(self)
+
+
+def print_banner(bot: TriggerBot):
+    print("Valorant TriggerBot - AustXnSheep")
+    print("====== controls ======")
+    print("TRIGGER: " + TRIGGER_KEY)
+    print("ENLARGE GRABZONE: " + GRABZONEUP)
+    print("REDUCE GRABZONE: " + GRABZONEDOWN)
+    print("====== INFO ======")
+    print("GRABZONE: " + str(GRABZONE))
+    print("SHOTSPEED: " + str(CURRENTMODE))
+
+
+if __name__ == "__main__":
+    bot = TriggerBot()
+    print_banner(bot)
+    while True:
+        if keyboard.is_pressed(TRIGGER_KEY):
+            bot.toggle()
+            print_banner(bot)
+            if bot.toggled:
+                winsound.Beep(440, 75)
+                winsound.Beep(700, 100)
+            else:
+                winsound.Beep(440, 75)
+                winsound.Beep(200, 100)
+            while keyboard.is_pressed(TRIGGER_KEY):
+                pass
+        if keyboard.is_pressed(GRABZONEUP):
+            GRABZONE = GRABZONE + 1
+            print_banner(bot)
+            winsound.Beep(440, 75)
+            winsound.Beep(700, 100)
+        if keyboard.is_pressed(GRABZONEDOWN):
+            GRABZONE = GRABZONE - 1
+            print_banner(bot)
+            winsound.Beep(440, 75)
+            winsound.Beep(200, 100)
+        if keyboard.is_pressed(MODECHANGEKEY):
+            bot.switch()
+            print_banner(bot)
+        if bot.toggled:
+            bot.scan()
+
+            # switch(self)
+#loops
+root.mainloop()
